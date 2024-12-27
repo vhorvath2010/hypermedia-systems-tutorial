4ca398ccc1547bb2ff82d379e30de5eb76e8d3ff
Vincent Horvath <vhorvath2010@gmail.com>
2024-12-27 00:08:32 -0500

    fix: proper layout html setup

--- templates/layout.ts
+++ templates/layout.ts
@@ -1,8 +1,16 @@
 import { HtmlEscapedString } from "@hono/hono/utils/html";
 import { html } from "@hono/hono/html";
 
-export function Layout(inner: HtmlEscapedString | Promise<HtmlEscapedString>) {
-  return html`<head>
-      <script src="https://unpkg.com/htmx.org@2.0.4"></script></head
-    >${inner}`;
+export function Layout(body: HtmlEscapedString | Promise<HtmlEscapedString>) {
+  return html`<html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Contact App</title>
+      <script src="https://unpkg.com/htmx.org@2.0.4"></script>
+    </head>
+    <body>
+      ${body}
+    </body>
+  </html>`;
 }
